acbc1dfb22f9c751878dc92fcbb06a535bb07dbe
Remy Vanherweghem <remyv@kle-en-main.com>
2014-07-02 11:39:00 -0400

    Added a database structure file used to instanciate new stores

---
+++ protected/dbstructure.sql
@@ -0,0 +1,362 @@
+-- phpMyAdmin SQL Dump
+-- version 4.1.12
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost:3306
+-- Generation Time: Jul 01, 2014 at 06:44 PM
+-- Server version: 5.1.73
+-- PHP Version: 5.4.16
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `boukem_demo`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `address`
+--
+
+CREATE TABLE IF NOT EXISTS `address` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `street1` text NOT NULL,
+  `street2` int(11) DEFAULT NULL,
+  `postcode` varchar(32) DEFAULT NULL,
+  `city` text NOT NULL,
+  `name` text,
+  `region` varchar(32) NOT NULL,
+  `country` varchar(32) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=11 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `category`
+--
+
+CREATE TABLE IF NOT EXISTS `category` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `parent_category` int(11) DEFAULT NULL,
+  `visible` tinyint(1) NOT NULL DEFAULT '1',
+  `is_brand` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  KEY `parent_category` (`parent_category`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=215 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `category_localization`
+--
+
+CREATE TABLE IF NOT EXISTS `category_localization` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `locale_id` varchar(2) NOT NULL,
+  `category_id` int(11) NOT NULL,
+  `name` text NOT NULL,
+  `visible` tinyint(1) NOT NULL DEFAULT '1',
+  `slug` varchar(1024) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `locale_id` (`locale_id`,`category_id`),
+  KEY `category_id` (`category_id`),
+  KEY `slug` (`slug`(255))
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=217 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `locale`
+--
+
+CREATE TABLE IF NOT EXISTS `locale` (
+  `id` varchar(2) NOT NULL,
+  `Name` text NOT NULL,
+  `long_code` varchar(10) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `order`
+--
+
+CREATE TABLE IF NOT EXISTS `order` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) DEFAULT NULL,
+  `cart` tinyint(1) NOT NULL DEFAULT '1',
+  `timestamp` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `status` varchar(64) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `timestamp` (`timestamp`,`status`),
+  KEY `user_id` (`user_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=53 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `order_details`
+--
+
+CREATE TABLE IF NOT EXISTS `order_details` (
+  `order_id` int(11) NOT NULL,
+  `subtotal` decimal(10,2) NOT NULL DEFAULT '0.00',
+  `taxes` decimal(10,2) NOT NULL DEFAULT '0.00',
+  `total` decimal(10,2) NOT NULL DEFAULT '0.00',
+  `shipping` decimal(10,2) NOT NULL DEFAULT '0.00',
+  `shipping_type` text NOT NULL,
+  `billing_address_id` int(11) NOT NULL,
+  `shipping_address_id` int(11) NOT NULL,
+  `balance` decimal(10,2) NOT NULL DEFAULT '0.00',
+  PRIMARY KEY (`order_id`),
+  KEY `billing_address_id` (`billing_address_id`,`shipping_address_id`),
+  KEY `shipping_address_id` (`shipping_address_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `order_has_product`
+--
+
+CREATE TABLE IF NOT EXISTS `order_has_product` (
+  `order_id` int(11) NOT NULL,
+  `product_id` int(11) NOT NULL,
+  `quantity` int(5) NOT NULL,
+  `price_paid` decimal(10,2) NOT NULL DEFAULT '0.00',
+  PRIMARY KEY (`order_id`,`product_id`),
+  KEY `product_id` (`product_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `product`
+--
+
+CREATE TABLE IF NOT EXISTS `product` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `sku` text NOT NULL,
+  `barcode` varchar(64) DEFAULT NULL,
+  `brand_id` int(11) NOT NULL COMMENT 'Brand',
+  `discontinued` tinyint(1) NOT NULL DEFAULT '0',
+  `visible` tinyint(1) NOT NULL DEFAULT '1',
+  `taxable` tinyint(1) NOT NULL DEFAULT '1',
+  `price` decimal(10,2) NOT NULL DEFAULT '0.00',
+  `weight` decimal(10,3) NOT NULL DEFAULT '0.010',
+  `parent_product_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `brand_id` (`brand_id`,`parent_product_id`),
+  KEY `parent_product_id` (`parent_product_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3253 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `product_has_category`
+--
+
+CREATE TABLE IF NOT EXISTS `product_has_category` (
+  `product_id` int(11) NOT NULL,
+  `category_id` int(11) NOT NULL,
+  PRIMARY KEY (`product_id`,`category_id`),
+  KEY `category_id` (`category_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `product_image`
+--
+
+CREATE TABLE IF NOT EXISTS `product_image` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `extension` text NOT NULL,
+  `identifier` int(11) NOT NULL,
+  `product_id` int(11) NOT NULL,
+  `position` int(2) NOT NULL DEFAULT '0',
+  `locale_id` varchar(2) NOT NULL DEFAULT 'fr',
+  PRIMARY KEY (`id`),
+  KEY `product_id` (`product_id`,`position`),
+  KEY `locale_id` (`locale_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3191 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `product_localization`
+--
+
+CREATE TABLE IF NOT EXISTS `product_localization` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `product_id` int(11) NOT NULL,
+  `locale_id` varchar(2) NOT NULL,
+  `name` text NOT NULL,
+  `long_description` text NOT NULL,
+  `short_description` text NOT NULL,
+  `visible` int(1) NOT NULL DEFAULT '1',
+  `slug` varchar(1024) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `locale_id` (`locale_id`),
+  KEY `url_key` (`slug`(255)),
+  KEY `product_id` (`product_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=5521 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `product_rebate`
+--
+
+CREATE TABLE IF NOT EXISTS `product_rebate` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `product_id` int(11) NOT NULL,
+  `price` decimal(10,2) NOT NULL DEFAULT '0.00',
+  `valid_from` date NOT NULL,
+  `valid_until` date NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `product_id` (`product_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `timestamp` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `email` varchar(254) NOT NULL,
+  `password` text,
+  `firstname` text,
+  `lastname` text,
+  `verification_string` text,
+  `locale_id` varchar(10) NOT NULL DEFAULT 'fr',
+  `postcode` text,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email_2` (`email`),
+  KEY `email` (`email`),
+  KEY `locale_id` (`locale_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=5 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_phone`
+--
+
+CREATE TABLE IF NOT EXISTS `user_phone` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `number` text NOT NULL,
+  `sms_opt_in` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4 ;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `address`
+--
+ALTER TABLE `address`
+  ADD CONSTRAINT `address_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `category`
+--
+ALTER TABLE `category`
+  ADD CONSTRAINT `category_ibfk_1` FOREIGN KEY (`parent_category`) REFERENCES `category` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `category_localization`
+--
+ALTER TABLE `category_localization`
+  ADD CONSTRAINT `category_localization_ibfk_1` FOREIGN KEY (`locale_id`) REFERENCES `locale` (`id`),
+  ADD CONSTRAINT `category_localization_ibfk_2` FOREIGN KEY (`category_id`) REFERENCES `category` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `order`
+--
+ALTER TABLE `order`
+  ADD CONSTRAINT `order_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
+
+--
+-- Constraints for table `order_details`
+--
+ALTER TABLE `order_details`
+  ADD CONSTRAINT `order_details_ibfk_1` FOREIGN KEY (`order_id`) REFERENCES `order` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `order_details_ibfk_2` FOREIGN KEY (`billing_address_id`) REFERENCES `address` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `order_details_ibfk_3` FOREIGN KEY (`shipping_address_id`) REFERENCES `address` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `order_has_product`
+--
+ALTER TABLE `order_has_product`
+  ADD CONSTRAINT `order_has_product_ibfk_1` FOREIGN KEY (`order_id`) REFERENCES `order` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `order_has_product_ibfk_2` FOREIGN KEY (`product_id`) REFERENCES `product` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `product`
+--
+ALTER TABLE `product`
+  ADD CONSTRAINT `Brand` FOREIGN KEY (`brand_id`) REFERENCES `category` (`id`),
+  ADD CONSTRAINT `product_ibfk_2` FOREIGN KEY (`parent_product_id`) REFERENCES `product` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `product_has_category`
+--
+ALTER TABLE `product_has_category`
+  ADD CONSTRAINT `product_has_category_ibfk_1` FOREIGN KEY (`product_id`) REFERENCES `product` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `product_has_category_ibfk_2` FOREIGN KEY (`category_id`) REFERENCES `category` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `product_image`
+--
+ALTER TABLE `product_image`
+  ADD CONSTRAINT `product_image_ibfk_1` FOREIGN KEY (`product_id`) REFERENCES `product` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `product_image_ibfk_2` FOREIGN KEY (`locale_id`) REFERENCES `locale` (`id`);
+
+--
+-- Constraints for table `product_localization`
+--
+ALTER TABLE `product_localization`
+  ADD CONSTRAINT `product_localization_ibfk_1` FOREIGN KEY (`locale_id`) REFERENCES `locale` (`id`),
+  ADD CONSTRAINT `product_localization_ibfk_2` FOREIGN KEY (`product_id`) REFERENCES `product` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `product_rebate`
+--
+ALTER TABLE `product_rebate`
+  ADD CONSTRAINT `product_rebate_ibfk_1` FOREIGN KEY (`product_id`) REFERENCES `product` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `user`
+--
+ALTER TABLE `user`
+  ADD CONSTRAINT `user_ibfk_1` FOREIGN KEY (`locale_id`) REFERENCES `locale` (`id`);
+
+--
+-- Constraints for table `user_phone`
+--
+ALTER TABLE `user_phone`
+  ADD CONSTRAINT `user_phone_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
