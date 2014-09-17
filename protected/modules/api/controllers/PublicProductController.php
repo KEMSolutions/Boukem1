cd3b4a3ccba219e34527f38e4121853e2355e3a4
Remy Vanherweghem <remyv@kle-en-main.com>
2014-09-17 16:15:33 -0400

    Added a single product view API

--- protected/modules/api/controllers/PublicProductController.php
+++ protected/modules/api/controllers/PublicProductController.php
@@ -3,7 +3,7 @@
 class PublicProductController extends APIController
 {
 	
-	protected $public_api = false;
+	protected $public_api = true;
 	protected $require_authentification = false;
 	
 	/**
@@ -20,25 +20,12 @@ class PublicProductController extends APIController
 	/**
 	 * View a particular product, with localizations and images associated.
 	 */
-	public function actionView()
+	public function actionView($id)
 	{
-
-		$payload = $this->extractJSON();
-		$product = $this->loadModel($payload);
-
-		$product_dict = array("id"=>$product->id, "sku"=>$product->sku, "barcode"=>$product->barcode, "brand_id"=>$product->brand_id, "discontinued"=>$product->discontinued, "visible"=>$product->visible, "taxable"=>$product->taxable, "price"=>$product->price, "weight"=>$product->weight, "parent_product_id"=>$product->parent_product_id);
-
-		$product_dict["localizations"] = array();
-
-		foreach ($product->productLocalizations as $productLocalization) {
-
-			$product_dict["localizations"][$productLocalization->locale_id] = array("id"=>$productLocalization->id, "name"=>$productLocalization->name, "long_description"=>$productLocalization->long_description, "short_description"=>$productLocalization->short_description, "slug"=>$productLocalization->slug);
 		
-		}
-
-
-		$this->renderJSON($product_dict);
-
+		$model = $this->loadModel($id);
+		$this->renderJSON($model->getStructuredProductArray());
+		
 	}
 	
 	
@@ -49,11 +36,11 @@ class PublicProductController extends APIController
 	 * @return Product the loaded model
 	 * @throws CHttpException
 	 */
-	public function loadModel($payload)
+	public function loadModel($id)
 	{
 		
 		
-		$model=Product::model()->findByPk($payload->id);
+		$model=Product::model()->findByPk($id);
 		if($model===null)
 			throw new CHttpException(404,'The requested page does not exist.');
 		return $model;
